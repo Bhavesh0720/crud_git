585e27234f078fd2d854f04eca2c97cf9dd259bd
Bhavesh0720 <bhavesh0720@gmail.com>
2025-08-13 12:55:57 +0530

    retrived data from model employee and showed

--- crud_git/myapp1/views.py
+++ crud_git/myapp1/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    uid = Employee.objects.all()
+    context = {
+        'uid':uid,
+    }
+    return render(request, 'index.html', context)
